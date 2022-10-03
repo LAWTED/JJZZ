0a028e4b81fac15f785ad75d5d552ed0a7c65e2d
Lawted <wmz@bupt.edu.cn>
2022-10-04 00:28:34 +0800

    feat: main page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Page,
   Navbar,
@@ -14,16 +14,39 @@ import {
   ListItem,
   Row,
   Col,
-  Button
-} from 'framework7-react';
+  Button,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Icon,
+} from "framework7-react";
 
 const HomePage = () => (
   <Page name="home">
-    {/* Top Navbar */}
-    <Navbar large transparent title='叽叽喳喳'></Navbar>
-    {/* Toolbar */}
-    {/* Page content */}
-    <BlockTitle>Navigation</BlockTitle>
+    <Navbar large transparent title="叽叽喳喳"></Navbar>
+    <List inset>
+      <ListItem title="All Chats" link="#">
+        <Icon slot="media" f7="chat_bubble_2" className="text-red-200"></Icon>
+      </ListItem>
+
+      <ListItem title="Stared Chats" link="#">
+        <Icon slot="media" f7="star"></Icon>
+      </ListItem>
+      <ListItem title="Top Chats" link="#">
+        <Icon slot="media" f7="chart_bar_alt_fill"></Icon>
+      </ListItem>
+    </List>
+
+    <BlockTitle medium>Others</BlockTitle>
+    <List inset>
+      <ListItem title="Settings" link="#">
+        <Icon slot="media" f7="gear"></Icon>
+      </ListItem>
+      <ListItem title="About" link="#">
+        <Icon slot="media" f7="info"></Icon>
+      </ListItem>
+    </List>
   </Page>
 );
 export default HomePage;
